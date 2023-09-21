f010aac0d93cec597fd032a383cbd44813592fd9
M <miladmasghatiyan1122@gmail.com>
2023-09-21 12:49:35 +0330

    edited

--- src/index.php
+++ src/index.php
@@ -1,6 +1,9 @@
 <?php
 
-copy(__DIR__."/Controller/MetaConfigController.php",dirname(__DIR__ , 4)."/src/Controller/"."MetaConfigController.php");
-copy(__DIR__."/Entity/MetaConfigs.php",dirname(__DIR__ , 4)."/src/Entity/"."MetaConfigs.php");
-copy(__DIR__."/Repository/MetaConfigsRepository.php",dirname(__DIR__ , 4)."/src/Repository/"."MetaConfigsRepository.php");
-copy(__DIR__."/Config/Types.php",dirname(__DIR__ , 4)."/src/Config/"."Types.php");
+if(!file_exists(__DIR__."/test")) {
+    copy(__DIR__ . "/Controller/MetaConfigController.php", dirname(__DIR__, 4) . "/src/Controller/" . "MetaConfigController.php");
+    copy(__DIR__ . "/Entity/MetaConfigs.php", dirname(__DIR__, 4) . "/src/Entity/" . "MetaConfigs.php");
+    copy(__DIR__ . "/Repository/MetaConfigsRepository.php", dirname(__DIR__, 4) . "/src/Repository/" . "MetaConfigsRepository.php");
+    copy(__DIR__ . "/Config/Types.php", dirname(__DIR__, 4) . "/src/Config/" . "Types.php");
+    mkdir(__DIR__ . "/test");
+}
