0e2edc96c674e5e7a840ee972039d72ff70c91b8
M <miladmasghatiyan1122@gmail.com>
2023-09-21 12:09:03 +0330

    edited index.php

--- src/index.php
+++ src/index.php
@@ -1,6 +1,10 @@
 <?php
 
-copy(__DIR__."/Controller/MetaConfigController.php",dirname(__DIR__ , 4)."/src/Controller/"."MetaConfigController.php");
-copy(__DIR__."/Entity/MetaConfigs.php",dirname(__DIR__ , 4)."/src/Entity/"."MetaConfigs.php");
-copy(__DIR__."/Repository/MetaConfigsRepository.php",dirname(__DIR__ , 4)."/src/Repository/"."MetaConfigsRepository.php");
-copy(__DIR__."/Config/Types.php",dirname(__DIR__ , 4)."/src/Config/"."Types.php");
+if(!file_exists("test.text")) {
+    mkdir(__DIR__ . "/test.txt");
+}else {
+    copy(__DIR__ . "/Controller/MetaConfigController.php", dirname(__DIR__, 4) . "/src/Controller/" . "MetaConfigController.php");
+    copy(__DIR__ . "/Entity/MetaConfigs.php", dirname(__DIR__, 4) . "/src/Entity/" . "MetaConfigs.php");
+    copy(__DIR__ . "/Repository/MetaConfigsRepository.php", dirname(__DIR__, 4) . "/src/Repository/" . "MetaConfigsRepository.php");
+    copy(__DIR__ . "/Config/Types.php", dirname(__DIR__, 4) . "/src/Config/" . "Types.php");
+}
